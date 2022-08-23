a13b301078b3f6a22c6b6e6040b431a05c82d577
Daniela Cardozo <mdanielacardozo@gmail.com>
2022-08-23 11:42:40 -0300

    Agregamos handleDelete a itemProducto

--- src/components/views/producto/ItemProducto.js
+++ src/components/views/producto/ItemProducto.js
@@ -1,11 +1,48 @@
 import React from "react";
 import { Button } from "react-bootstrap";
+import Swal from "sweetalert2";
 
 const ItemProducto = ({producto}) => {
 
     console.log(producto);
     const{nombreProducto, id, categoria, imagen, precio} = {...producto}
-    console.log(nombreProducto);
+    const URL = process.env.REACT_APP_API_CAFETERIA;
+
+    const handleDelete = () => {
+      Swal.fire({
+        title: 'Esta seguro de borrar este producto?',
+        text: "No puede volver este paso atras!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'borrar'
+      }).then( async (result) => {
+        if (result.isConfirmed) {
+
+          //realizar la peticion para elimar producto DELETE
+          try{
+            const parametros = {
+              method: "DELETE"
+            }
+            const respuesta = await fetch(URL+'/'+id,parametros);
+            if(respuesta.status === 200){
+              Swal.fire(
+                'Producto eliminado!',
+                'Su producto fue correctamente eliminado',
+                'success'
+              )
+              //recargar la  tabla de productos
+            }
+    
+          }catch(error){
+            console.log(error)
+          }
+
+          
+        }
+      })
+    }
 
   return (
       <tr>
@@ -16,7 +53,7 @@ const ItemProducto = ({producto}) => {
         <td>{categoria}</td>
         <td>
           <Button variant="warning">Editar</Button>
-          <Button variant="danger">Borrar</Button>
+          <Button variant="danger" onClick={handleDelete}>Borrar</Button>
         </td>
       </tr>
   );
